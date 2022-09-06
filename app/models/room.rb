47f58314ea55c92b375ffbecbacbaaa6ffdc93ed
CristianRojasBetancur <cristianrojasbetancur@gmail.com>
2022-09-06 17:09:16 -0500

    Validando habitaciones

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,8 @@
 class Room < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true, length: { maximum: 400 }
+  validates :beds, presence: true, numericality: {only_integer: true}
+  validates :guests, presence: true, numericality: {only_integer: true}
+  validates :image_url, presence: true
+  validates :price_per_night, presence: true
 end
